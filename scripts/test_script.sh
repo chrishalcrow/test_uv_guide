4ddd98adb4bb80946d81a774fd98096386b120e3
chrishalcrow <chrishalcrow@gmail.com>
2025-07-02 13:06:33 +0100

    fix bug

--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #$ -cwd -pe sharedmem 1 -l h_vmem=19G,h_rt=0:29:59 -N my_job
 
-cd /exports/eddie/scratch/chalcrow/path/to/my_project
+cd /exports/eddie/scratch/chalcrow/test_uv_guide/
 /home/chalcrow/.local/bin/uv run main_script.py
