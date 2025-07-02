f3df0cf06c1a39327cba8a46adbeb995a0998b35
chrishalcrow <chrishalcrow@gmail.com>
2025-07-02 13:07:31 +0100

    oups

--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -2,4 +2,4 @@
 #$ -cwd -pe sharedmem 1 -l h_vmem=19G,h_rt=0:29:59 -N my_job
 
 cd /exports/eddie/scratch/chalcrow/test_uv_guide/
-/home/chalcrow/.local/bin/uv run main_script.py
+/home/chalcrow/.local/bin/uv run script.py
